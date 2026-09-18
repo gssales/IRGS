570992f765837570f87045137cfb324425963f3e
Guilherme Sales <guilherme.sales@inf.ufrgs.br>
2026-09-18 14:50:29 -0300

    fix dataset reader

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -100,7 +100,7 @@ def readColmapCameras(cam_extrinsics, cam_intrinsics, images_folder):
                 [0, focal_length_x, intr.params[2]],
                 [0, 0, 1],
             ])
-        elif intr.model=="PINHOLE":
+        elif intr.model=="PINHOLE" or intr.model == "OPENCV":
             focal_length_x = intr.params[0]
             focal_length_y = intr.params[1]
             FovY = focal2fov(focal_length_y, height)
